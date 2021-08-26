d486078c2b3003042ed27592ce15c56cdf4a5144
Linus Pahl <linus@graylog.com>
2021-08-26 18:42:22 +0200

    Create `MessageDetailContextProviders` which renders plugable context provider.

--- graylog2-web-interface/src/views/components/messagelist/MessageDetail.tsx
+++ graylog2-web-interface/src/views/components/messagelist/MessageDetail.tsx
@@ -36,6 +36,7 @@ import { FieldTypeMappingsList } from 'views/stores/FieldTypesStore';
 import { useStore } from 'stores/connect';
 import { SearchConfigStore } from 'views/stores/SearchConfigStore';
 
+import MessageDetailProviders from './MessageDetailProviders';
 import NodeName from './NodeName';
 import MessageActions from './MessageActions';
 import MessageAugmentations from './MessageAugmentations';
@@ -166,41 +167,43 @@ const MessageDetail = ({
   const messageTitle = _formatMessageTitle(index, id);
 
   return (
-    <>
-      <Row className="row-sm">
-        <Col md={12}>
-          <MessageActions index={index}
-                          id={id}
-                          fields={fields}
-                          decorationStats={decorationStats}
-                          disabled={disableMessageActions}
-                          disableSurroundingSearch={disableSurroundingSearch}
-                          disableTestAgainstStream={disableTestAgainstStream}
-                          showOriginal={showOriginal}
-                          toggleShowOriginal={_toggleShowOriginal}
-                          searchConfig={searchesClusterConfig}
-                          streams={allStreams} />
-          <MessageDetailsTitle>
-            <Icon name="envelope" />
-            &nbsp;
-            {messageTitle}
-          </MessageDetailsTitle>
-        </Col>
-      </Row>
-      <Row id={`sticky-augmentations-boundary-${message.id}`}>
-        <Col md={3}>
-          <MessageMetadata timestamp={timestamp}
-                           index={index}
-                           receivedBy={<FormatReceivedBy inputs={inputs} sourceNodeId={gl2_source_node} sourceInputId={gl2_source_input} />}
-                           streams={streamsListItems} />
-          <MessageAugmentations message={message} />
-        </Col>
-        <Col md={9}>
-          <MessageFields message={message}
-                         fields={messageFields} />
-        </Col>
-      </Row>
-    </>
+    <MessageDetailProviders message={message}>
+      <>
+        <Row className="row-sm">
+          <Col md={12}>
+            <MessageActions index={index}
+                            id={id}
+                            fields={fields}
+                            decorationStats={decorationStats}
+                            disabled={disableMessageActions}
+                            disableSurroundingSearch={disableSurroundingSearch}
+                            disableTestAgainstStream={disableTestAgainstStream}
+                            showOriginal={showOriginal}
+                            toggleShowOriginal={_toggleShowOriginal}
+                            searchConfig={searchesClusterConfig}
+                            streams={allStreams} />
+            <MessageDetailsTitle>
+              <Icon name="envelope" />
+              &nbsp;
+              {messageTitle}
+            </MessageDetailsTitle>
+          </Col>
+        </Row>
+        <Row id={`sticky-augmentations-boundary-${message.id}`}>
+          <Col md={3}>
+            <MessageMetadata timestamp={timestamp}
+                             index={index}
+                             receivedBy={<FormatReceivedBy inputs={inputs} sourceNodeId={gl2_source_node} sourceInputId={gl2_source_input} />}
+                             streams={streamsListItems} />
+            <MessageAugmentations message={message} />
+          </Col>
+          <Col md={9}>
+            <MessageFields message={message}
+                           fields={messageFields} />
+          </Col>
+        </Row>
+      </>
+    </MessageDetailProviders>
   );
 };
 
@@ -229,5 +232,3 @@ MessageDetail.defaultProps = {
   showTimestamp: true,
   streams: Immutable.Map(),
 };
-
-export default MessageDetail;
